2103698550c186b9808878ad271327cc1b510389
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2026-07-18 14:40:26 +0900

    テスト環境調整

--- Source/App/Designer.WpfApp/DbAccessorFactory.cs
+++ Source/App/Designer.WpfApp/DbAccessorFactory.cs
@@ -1,7 +1,7 @@
 ﻿using Codeer.LowCode.Blazor.DataIO.Db;
 using Codeer.LowCode.Blazor.Designer;
 using Codeer.LowCode.Blazor.SystemSettings;
-using WebApp.Server.Shared;
+using Codeer.LowCode.Blazor.DbAccess;
 
 namespace Designer.WpfApp
 {
